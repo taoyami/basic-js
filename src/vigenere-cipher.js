4576ab951af8bee812fedb66eee3c342119f8122
Tatyana Savina <61759480+taoyami@users.noreply.github.com>
2023-04-17 23:16:30 +0300

    feat: vigenere-cipher solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+	constructor(type = true) {
+		this.type = type;
+	}
+
+	encrypt(text, word) { 
+		if (text === undefined || word === undefined || typeof text !== "string" || typeof word !== "string") throw new Error ("Incorrect arguments!");
+		text = text.toUpperCase();
+		word = word.toUpperCase();
+		let key = word.repeat(Math.ceil(text.length / word.length)),
+			  codeA = "A".charCodeAt(0),
+			  res = [];
+
+		for (let i = 0, j = 0; i < text.length; i++, j++) {
+			if (!"ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(text[i])) {
+				res.push(text[i]);
+				j--;
+			} else {
+				let letterI = text.charCodeAt(i) - codeA,
+					  shift = key.charCodeAt(j) - codeA;
+				    res.push( String.fromCharCode( codeA + (letterI + shift) % 26 ) );
+			}
+		}
+
+		return this.type ? res.join('') : res.reverse().join('');
+	}
+
+	decrypt(text, word) {
+		if (text === undefined || word === undefined || typeof text !== "string" || typeof word !== "string") throw new Error ("Incorrect arguments!");
+		
+		text = text.toUpperCase();
+		word = word.toUpperCase();
+		let key = word.repeat(Math.ceil(text.length / word.length)),
+			  codeA = "A".charCodeAt(0),
+			  res = [];
+
+		for (let i = 0, j = 0; i < text.length; i++, j++) {
+			if (!"ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(text[i])) {
+				res.push(text[i]);
+				j--;
+			} else {
+				let letterI = text.charCodeAt(i) - codeA,
+					  shift = key.charCodeAt(j) - codeA;
+				res.push( String.fromCharCode( codeA + (letterI - shift + 26) % 26 ) );
+			}
+		}
+
+		return this.type ? res.join('') : res.reverse().join('');
+	}
 }
 
 module.exports = {
